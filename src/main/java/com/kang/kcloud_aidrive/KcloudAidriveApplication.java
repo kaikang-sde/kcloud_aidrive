35298b800e8ded7eaba75fc77026941487cf56cf
kai kang <kaikangsde@gmail.com>
2025-01-12 11:26:31 -0500

    init

---
+++ src/main/java/com/kang/kcloud_aidrive/KcloudAidriveApplication.java
@@ -0,0 +1,13 @@
+package com.kang.kcloud_aidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcloudAidriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcloudAidriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kang/kcloud_aidrive/KcloudAidriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kang.kcloud_aidrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcloudAidriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
